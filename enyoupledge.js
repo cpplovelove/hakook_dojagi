2933ff4842d09efd2eff878542ec42277cf3b9a4
s2코딩천사선영이s2 <57889360+cpplovelove@users.noreply.github.com>
2021-05-29 15:30:28 +0900

    Update enyoupledge.js

--- enyoupledge.js
+++ enyoupledge.js
@@ -2,7 +2,7 @@ $(function() {
 
 	$("#test1").click(function () {
 		$(".post-wrapper").css('display','none');
-	 	document.getElementById("result_img").src = "test1.JPG";
+	 	document.getElementById("result_img").src = "test1.jpg";
 
 		$("#text1").text("type A, white bloom");
 		$("#text2").text("심플하고 단조로운 분위기를 선호하는 당신!나긋나긋하고 부드러운 분위기 뒤에는 강직함이 숨어있어요. 타인과 스스럼 없이 잘어울리며, 단순한 사실에 입각한 딱딱한 대화보다는 섬세한 언어를 사용해서 대화를 나누는 것을 즐기기도 합니다.주변에게 편안한 분위기를 선물하는 당신에게는 항상 사람이 끊이지 않을 거에요. ");
@@ -18,7 +18,7 @@ $(function() {
 	$("#test2").click(function () {
 		$(".post-wrapper").css('display','none');
 		
-	 	document.getElementById("result_img").src = "test2.JPG";
+	 	document.getElementById("result_img").src = "test2.jpg";
 		$("#text1").text("type B, royal PaleBlue");
 		$("#text2").text("타고난 낙관주의자로 밝은 분위기로 주변을 즐겁게 해주는 당신! 사람들의 장점을 발견하고 칭찬하는데 능숙해요. 소소한 선물이나 좋아하는 사람의 웃음만으로도 행복감을 느끼는 사람이네요.흩어져 있는 사람들을 한데 모으는 재능을 가졌어요, 선물 같은 사람이네요!");
 		
@@ -31,7 +31,7 @@ $(function() {
 	
 	$("#test3").click(function () {
 		$(".post-wrapper").css('display','none');
-	 	document.getElementById("result_img").src = "test3.JPG";
+	 	document.getElementById("result_img").src = "test3.jpg";
 		
 		$("#text1").text("type C, cozy blue");
 		$("#text2").text("신중하고 끈기 있으며, 냉정하게 상황을 판단할 수 있는 당신! 이루고 싶은 목표가 있다면 확실하게 만들기 위한 계획을 세워나가기 시작해요. 일에 집중하면 주위가 잘 보이지 않는 당신! 노력하는 모습에 주위 사람들은 당신을 신뢰하고 존경해요");
@@ -45,7 +45,7 @@ $(function() {
 	
 	$("#test4").click(function () {
 		$(".post-wrapper").css('display','none');
-	 	document.getElementById("result_img").src = "test4.JPG";
+	 	document.getElementById("result_img").src = "test4.jpg";
 		
 		$("#text1").text("type D, Ole Jensen");
 		$("#text2").text("적극적이고 진취적인 모습으로 4차원적인 매력을 가진 당신! 평범한 것을 좋아하지 않는 당신은 주관이 뚜렷한 성격으로 개성 넘치는 사람이에요. 예쁘고 화려한 것들로 자신을 포장할 줄 알면서, 주변 사람들에게 센스있는 사람으로 손에 꼽히네요!");
